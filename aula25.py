93a6a24bb5f8f0e423e21bbfa422dce7c07191fe
tavaresdev-2210 <lucas.t.rosa@outlook.com>
2026-06-25 17:32:48 -0300

    Add files via upload

---
+++ aula25.py
@@ -0,0 +1,11 @@
+#Operador lógico "not"
+# Usado para inverter expressões 
+# not True = False 
+# False = True
+#Se digitar algo não acontece nada pois nao existe nenhuma condição 
+senha= input('Senha: ')
+if not senha:
+    print('Você acabou não digitou ')
+print(not True)#False
+print(not False)#True
+  
